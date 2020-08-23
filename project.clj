5aef32a4a1abf3f3ad8693d81ec8cc03fe8b7106
Aubrey Snider <aubrey@snider.pw>
2020-08-23 16:24:11 +0000

    added sum endpoint

--- project.clj
+++ project.clj
@@ -7,8 +7,10 @@
                  [ring/ring-core "1.8.1"]
                  [ring/ring-jetty-adapter "1.8.1"]
                  [compojure "1.6.2"]
+                 [org.clojure/data.json "1.0.0"]
                  [org.clojure/java.jdbc "0.7.11"]
                  [org.postgresql/postgresql "42.2.14"]
                  [clojure.jdbc/clojure.jdbc-c3p0 "0.3.3"]]
   :repl-options {:init-ns hello-clojure.core}
-  :main hello-clojure.core/-main)
+  :main hello-clojure.core
+  :aot [hello-clojure.core])
--- src/hello_clojure/core.clj
+++ src/hello_clojure/core.clj
@@ -1,6 +1,7 @@
 (ns hello-clojure.core
   (:use compojure.core)
   (:require [hello-clojure.db :as db]
+            [clojure.data.json :as json]
             [clojure.java.jdbc :as jdbc]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.util.response :as response]
@@ -9,12 +10,34 @@
 
 (defn getDBTime []
   {:status 200
-      :headers {"Content-Type" "text/plain"}
-      :body (str "DB localtimestamp: " (get (first (jdbc/query db/spec ["select localtimestamp"]))
-                                            :localtimestamp))})
+   :headers {"Content-Type" "application/json"}
+   :body (json/write-str {:request {:operation "time"}
+                          :response {:localtimestamp (str (get (first (jdbc/query db/spec ["select localtimestamp"]))
+                                                               :localtimestamp))}})})
+
+(defn getDBSum [x y]
+  {:status 200
+      :headers {"Content-Type" "application/json"}
+      :body (json/write-str {:request {:operation "sum" :first_operand x :second_operand y}
+                             :response {:sum (second (first (first (jdbc/query db/spec ["select ? + ?" x y]))))}})})
+
+(def apiInfoView "
+<html>
+  <head>
+  <title>Hello Clojure</title>
+  </head>
+  <body>
+     <p>Welcome to the app.</p>
+     <p>Go GET <a href='/time'>/time</a></p>
+     <p>Or go GET <a href='/sum-2-int/42/58'>/sum-2-int/42/58</a></p>
+  </body
+</html>
+")
 
 (defroutes app-routes
-  (GET "/" [] (getDBTime))
+  (GET "/" [] apiInfoView)
+  (GET "/time" [] (getDBTime))
+  (GET "/sum-2-int/:x/:y" [x y] (getDBSum (Integer/parseInt  x) (Integer/parseInt  y)))
   (route/resources "/")
   (route/not-found "404: Not Found"))
 
